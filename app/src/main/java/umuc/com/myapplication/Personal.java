1da11ddb5bcc70b56dc7be10816f19ddea9bac6b
Biancaj0929 <biancaj0929@gmail.com>
2016-04-13 20:07:47 -0400

    Base java classes renamed and created with basic onCreate methods

---
+++ app/src/main/java/umuc/com/myapplication/Personal.java
@@ -0,0 +1,62 @@
+package umuc.com.myapplication;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class Personal extends Goals {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText inputField = new EditText(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Selected Add", "New goal will be created");
+
+                builder.setTitle("Making a new goal!");
+                builder.setMessage("What is your new bucket list item?");
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("Personal",inputField.getText().toString());
+                    }
+                });
+
+                builder.setNegativeButton("Cancel",null);
+
+                builder.create().show();
+
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+}
