d394d306648b565602c7094d960396516f4f235b
steve-495 <__steve__@live.com>
2016-04-14 01:22:40 -0500

    Splash Screen

---
+++ app/src/main/java/umuc/com/myapplication/SplashScreen.java
@@ -0,0 +1,42 @@
+package umuc.com.myapplication;
+// test
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by Steve on 4/14/2016.
+ * Creates thread and puts thread to
+ *  sleep (pauses thread) for n milliseconds
+ *
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(12000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,Goals.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
+
+
