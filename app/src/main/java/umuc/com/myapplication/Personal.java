caa4d17166f621dcac75b22df00d5a48d385a1a2
Phillipmpayne <phillipmpayne@yahoo.com>
2016-04-15 13:47:41 +0200

    Crash fix

--- app/src/main/java/umuc/com/myapplication/Personal.java
+++ app/src/main/java/umuc/com/myapplication/Personal.java
@@ -3,6 +3,7 @@ package umuc.com.myapplication;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -11,18 +12,19 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class Personal extends Goals {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        final EditText inputField = new EditText(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus);
         assert fab != null;
@@ -30,21 +32,7 @@ public class Personal extends Goals {
             @Override
             public void onClick(View view) {
                 Log.d("Selected Add", "New goal will be created");
-
-                builder.setTitle("Making a new personal goal!");
-                builder.setMessage("What is your new bucket list item?");
-                builder.setView(inputField);
-                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("Personal",inputField.getText().toString());
-                    }
-                });
-
-                builder.setNegativeButton("Cancel",null);
-
-                builder.create().show();
-
+                displayPopup();
             }
         });
 
@@ -58,5 +46,22 @@ public class Personal extends Goals {
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    public void displayPopup() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(Personal.this);
+        final EditText inputField = new EditText(Personal.this);
+        builder.setTitle("Making a new goal!");
+        builder.setMessage("What is your new bucket list item?");
+        builder.setView(inputField);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("Goals",inputField.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton("Cancel",null);
+
+        builder.create().show();
+    }
 
 }
