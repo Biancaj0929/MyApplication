2e17777255cca9d58e72e2eec484b679da716494
Phillipmpayne <phillipmpayne@yahoo.com>
2016-04-15 13:48:29 +0200

    Crash fix

--- app/src/main/java/umuc/com/myapplication/Physical.java
+++ app/src/main/java/umuc/com/myapplication/Physical.java
@@ -1,7 +1,9 @@
 package umuc.com.myapplication;
 
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -10,19 +12,19 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
-
 
 public class Physical extends Goals {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physical);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        final EditText inputField = new EditText(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus);
         assert fab != null;
@@ -30,21 +32,7 @@ public class Physical extends Goals {
             @Override
             public void onClick(View view) {
                 Log.d("Selected Add", "New goal will be created");
-
-                builder.setTitle("Making a new physical goal!");
-                builder.setMessage("What is your new bucket list item?");
-                builder.setView(inputField);
-                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("Physical",inputField.getText().toString());
-                    }
-                });
-
-                builder.setNegativeButton("Cancel",null);
-
-                builder.create().show();
-
+                displayPopup();
             }
         });
 
@@ -58,5 +46,21 @@ public class Physical extends Goals {
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    public void displayPopup() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(Physical.this);
+        final EditText inputField = new EditText(Physical.this);
+        builder.setTitle("Making a new goal!");
+        builder.setMessage("What is your new bucket list item?");
+        builder.setView(inputField);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("Goals",inputField.getText().toString());
+            }
+        });
 
-}
+        builder.setNegativeButton("Cancel",null);
+
+        builder.create().show();
+    }
+
