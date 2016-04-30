fcfdacd9d450bb080706f682efdcd5960aec499f
Biancaj0929 <biancaj0929@gmail.com>
2016-04-30 00:48:57 -0400

    Steve - IO File Changes

--- app/src/main/java/umuc/com/myapplication/Personal.java
+++ app/src/main/java/umuc/com/myapplication/Personal.java
@@ -13,8 +13,11 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -31,22 +34,13 @@ import java.io.OutputStreamWriter;
 
 public class Personal extends Goals {
 
-    /* create text file to store list of personal goal names */
-    public final static String PERSLABELS = "perslabels.txt";
-
-    /* create text file to store list of dates for personal goals */
-    public final static String PERSDATES = "persdates.txt";
-
-
-
-    // On Create method sets activity layout for personal menu
+    // On Create method sets activity layout for spiritual menu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         // Floating action button to add a new goal, on click listener calls popup method
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus);
@@ -55,7 +49,7 @@ public class Personal extends Goals {
             @Override
             public void onClick(View view) {
                 Log.d("Selected Add", "New personal goal will be created");
-                displaycal();
+                displayCal();
             }
         });
 
@@ -68,40 +62,73 @@ public class Personal extends Goals {
         // Sets navigation view
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(Personal.this);
+
+        // Writes text file data to array
+        writeArray("pers.txt", personalArray);
+        // Updates goals list
+        updateGoalList(personalArray);
+    }
+
+    // Updates goal group list view
+    // Note: Had to recreate new array with only non-null elements. Feeding array otherwise to
+    //       ListView was crashing the app
+    public void updateGoalList(String[] goals) {
+        int count = 0;
+
+        for (int i = 0; i < goals.length; i++) {
+            if (goals[i] != null) {
+                count++;
+            }
+        }
+        // Creates new string array
+        String goals1[] = new String[count];
+
+        for (int i = 0 ; i < goals.length ; i++)
+            if (goals[i] != null) {
+                goals1[i] = goals[i];
+            }
+
+        final ArrayAdapter<String> theAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, goals1);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(theAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                String goalPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(position));
+
+
+                Toast.makeText(Personal.this, goalPicked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     // Displays date picker to keep track of deadlines for goals
-    public void displaycal() {
+    public void displayCal() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        DatePicker picker = new DatePicker(this);
-        final EditText DateInput = new EditText(Personal.this);
+        final DatePicker picker = new DatePicker(this);
+        //final DatePicker dp = (DatePicker) findViewById(R.id.dp);
 
-
-        builder.setTitle("Set your Deadline for your new Goal");
+        builder.setTitle("             Set a Deadline for" + "\n                 your new Goal");
         builder.setView(picker);
         builder.setNegativeButton("Cancel", null);
         builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                displayPopup();
-                Log.d("Personal Goals", DateInput.getText().toString());
-                try {
-                    OutputStreamWriter out =
-                            new OutputStreamWriter(openFileOutput(PERSDATES, MODE_APPEND));
-                    out.write(DateInput.getText().toString() + '\n');
-                    out.close();
-                    Toast.makeText(getApplicationContext(), "Date saved in the file.",
-                            Toast.LENGTH_LONG).show();
 
-                } catch (Throwable t) {
-                    Toast.makeText(getApplicationContext(), "Exception: " + t.toString(),
-                            Toast.LENGTH_LONG).show();
-                }
+                String month = String.valueOf(picker.getMonth() + 1);
+                String day = String.valueOf(picker.getDayOfMonth());
+                String year = String.valueOf(picker.getYear());
+                dateData = month + "-" + day + "-" + year;
+                displayPopup();
             }
         });
         builder.show();
     }
 
-    // Creates and displays alert dialog builder
+    // Creates and displays alert dialog builder (popup to enter goal name)
     public void displayPopup() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(Personal.this);
         final EditText inputField = new EditText(Personal.this);
@@ -111,26 +138,25 @@ public class Personal extends Goals {
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Log.d("Personal Goals", inputField.getText().toString() + '\n');
-                try {
-                    OutputStreamWriter out =
-                            new OutputStreamWriter(openFileOutput(PERSLABELS, MODE_APPEND));
-                    out.write(inputField.getText().toString() + "\n");
-                    out.close();
-                    Toast.makeText(getApplicationContext(), "Personal goal saved to file.",
-                            Toast.LENGTH_LONG).show();
 
-                } catch (Throwable t) {
-                    Toast.makeText(getApplicationContext(), "Exception: " + t.toString(),
-                            Toast.LENGTH_LONG).show();
-                }
+                Log.d("Personal Goals", goalData = inputField.getText().toString());
+                newGoalData = (goalData + "~" + dateData);
+
+                Toast.makeText(getApplicationContext(),"Goal ("+ goalData + ") " +
+                        "Saved",Toast.LENGTH_LONG).show();
+
+                // Write goal and date string data to file spiritual.txt
+                writeData("pers.txt", newGoalData);
+
+                // Writes text file data to array
+                writeArray("pers.txt", personalArray);
+                // Updates goals list
+                updateGoalList(personalArray);
 
             }
         });
-
-        builder.setNegativeButton("Cancel", null);
-
+        // *** Clear date variable at this point
+        builder.setNegativeButton("Cancel", null ); //{ dateData = "";}
         builder.create().show();
     }
-
 }
